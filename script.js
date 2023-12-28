1119825abd74299cde1eff265ae0166c273781d9
lancegabgab <lancegabcruz@gmail.com>
2023-12-28 15:59:51 +0800

    added jquery animations

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 $(function () {
 
+  $("#seemore1").click(()=>{
+    $("#start").slideToggle(1000);
+
+  })
+
 });
+
+
+
+$(function () {
+
+   $("#seemore2").click(()=>{
+      $("#met").slideToggle(1000);
+
+  })
+
+});
+
+//fadeIn when hovered
+$(function () {
+
+  var galleryItems = $(".gallery").find("img")
+  galleryItems.css("width", "33%").css("opacity", "0.7");
+
+  galleryItems.mouseenter(function(){
+    $(this).stop().fadeTo(500, 1)
+  })
+  galleryItems.mouseleave(function(){
+    $(this).stop().fadeTo(500, 0.7)
+  })
+});
+
